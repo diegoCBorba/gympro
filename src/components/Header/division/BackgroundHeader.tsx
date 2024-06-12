c20b158fd614517cd525eaa4b75aca6622e0508f
Diego Cardoso <90847079+diegoCBorba@users.noreply.github.com>
2024-06-12 15:58:41 -0300

    Alterações na header

--- src/components/Header/division/BackgroundHeader.tsx
+++ src/components/Header/division/BackgroundHeader.tsx
@@ -2,25 +2,22 @@ import header from "../Header.module.css"
 
 import { motion, MotionValue, useTransform } from "framer-motion"
 
-import imageHeader from "../../../../public/home/header/header-man.png"
+import imageHeader from "../../../../public/home/header/header-photo.jpg"
 
 interface PropsBackground{
   scrollYProgress: MotionValue<number>,
 }
 
 export const BackgroundHeader = ({scrollYProgress}: PropsBackground) => {
-  const height = useTransform(scrollYProgress, [0, 1], ["90%", "80%"]);
+  const height = useTransform(scrollYProgress, [0, 1], ["100%", "105%"]);
   const bottom = useTransform(scrollYProgress, [0, 1], ["0%", "-15%"]);
 
   return (
-    <div className={header.boxImageHeader}>
-      <motion.img 
-      initial={{height:"100%"}}
-      animate={{height:"90%"}}
-      transition={{duration: 0.5}}
-      style={{bottom, height}} 
-      src={imageHeader} 
-      alt="Homem Forte"/>
-    </div>
+    <motion.div className={header.boxImageHeader}
+      initial={{height: "105%"}}
+      animate={{height: "100%"}}
+      transition={{duration: 1}}
+      style={{backgroundImage: `url(${imageHeader})`, height, bottom}}
+    />
   )
 }
