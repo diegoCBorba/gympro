6285932b0f0e10652f01a93bb9866d7c3b504774
Diego Cardoso <90847079+diegoCBorba@users.noreply.github.com>
2024-06-13 21:02:43 -0300

    ajustes contato

--- src/components/Contact/division/SubtitleContact.tsx
+++ src/components/Contact/division/SubtitleContact.tsx
@@ -1,10 +1,18 @@
+import { SenteceTranslate } from "../../SentenceTranslate/SentenceTranslate"
 import contact from "../Contact.module.css"
 
 export const SubtitleContact = () => {
   return (
     <div className={contact.subtitleBackground}>
-      <h2>Conecte-se com</h2>
-      <h2>o seu <span>futuro</span></h2>
+      <h2>
+        <SenteceTranslate sentence="Conecte-se com" velocity={0}/>
+      </h2>
+      <h2>
+        <SenteceTranslate sentence="o seu" velocity={0}/>
+        <span className={contact.color}>
+        <SenteceTranslate sentence=" futuro" velocity={0}/>
+        </span>
+      </h2>
     </div>
   )
 }
